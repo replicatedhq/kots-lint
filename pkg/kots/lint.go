0aeffcd5b8785fd318903e5e86e0e916ecfb02d4
Craig O'Donnell <craig@replicated.com>
2023-05-26 17:34:05 -0400

    remove some indents

--- pkg/kots/lint.go
+++ pkg/kots/lint.go
@@ -501,89 +501,93 @@ func lintResourceAnnotations(specFiles SpecFiles) ([]LintExpression, error) {
 			return nil, errors.Wrap(err, "failed to unmarshal spec content")
 		}
 
-		if metadata, ok := doc["metadata"].(map[interface{}]interface{}); ok {
-			if annotations, ok := metadata["annotations"].(map[interface{}]interface{}); ok {
-				for k, v := range annotations {
-					// convert the key and value to strings
-					key, value := fmt.Sprintf("%v", k), fmt.Sprintf("%v", v)
-					switch key {
-					case kotsoperatortypes.CreationPhaseAnnotation, kotsoperatortypes.DeletionPhaseAnnotation:
-						// check that the value is a parsable integer between -9999 and 9999
-						parsed, err := strconv.ParseInt(value, 10, 64)
-						if err != nil {
-							lintExpression := LintExpression{
-								Rule:    "deployment-phase-annotation",
-								Type:    "error",
-								Path:    spec.Path,
-								Message: fmt.Sprintf("Resource annotation %s should be an integer", key),
-							}
-							lintExpressions = append(lintExpressions, lintExpression)
-						} else if parsed < -9999 || parsed > 9999 {
-							lintExpression := LintExpression{
-								Rule:    "deployment-phase-annotation",
-								Type:    "error",
-								Path:    spec.Path,
-								Message: fmt.Sprintf("Resource annotation %s should be between -9999 and 9999", key),
-							}
-							lintExpressions = append(lintExpressions, lintExpression)
-						}
-					case kotsoperatortypes.WaitForPropertiesAnnotation:
-						// check that the value is a comma separated list of key=value pairs
-						// where the key is a valid jsonpath and the value is not empty
-						if value == "" {
-							lintExpression := LintExpression{
-								Rule:    "wait-for-properties-annotation",
-								Type:    "error",
-								Path:    spec.Path,
-								Message: fmt.Sprintf("Resource annotation %s should not be empty", key),
-							}
-							lintExpressions = append(lintExpressions, lintExpression)
-							break
-						}
+		metadata, ok := doc["metadata"].(map[interface{}]interface{})
+		if !ok {
+			continue
+		}
+		annotations, ok := metadata["annotations"].(map[interface{}]interface{})
+		if !ok {
+			continue
+		}
+		for k, v := range annotations {
+			// convert the key and value to strings
+			key, value := fmt.Sprintf("%v", k), fmt.Sprintf("%v", v)
+			switch key {
+			case kotsoperatortypes.CreationPhaseAnnotation, kotsoperatortypes.DeletionPhaseAnnotation:
+				// check that the value is a parsable integer between -9999 and 9999
+				parsed, err := strconv.ParseInt(value, 10, 64)
+				if err != nil {
+					lintExpression := LintExpression{
+						Rule:    "deployment-phase-annotation",
+						Type:    "error",
+						Path:    spec.Path,
+						Message: fmt.Sprintf("Resource annotation %s should be an integer", key),
+					}
+					lintExpressions = append(lintExpressions, lintExpression)
+				} else if parsed < -9999 || parsed > 9999 {
+					lintExpression := LintExpression{
+						Rule:    "deployment-phase-annotation",
+						Type:    "error",
+						Path:    spec.Path,
+						Message: fmt.Sprintf("Resource annotation %s should be between -9999 and 9999", key),
+					}
+					lintExpressions = append(lintExpressions, lintExpression)
+				}
+			case kotsoperatortypes.WaitForPropertiesAnnotation:
+				// check that the value is a comma separated list of key=value pairs
+				// where the key is a valid jsonpath and the value is not empty
+				if value == "" {
+					lintExpression := LintExpression{
+						Rule:    "wait-for-properties-annotation",
+						Type:    "error",
+						Path:    spec.Path,
+						Message: fmt.Sprintf("Resource annotation %s should not be empty", key),
+					}
+					lintExpressions = append(lintExpressions, lintExpression)
+					break
+				}
 
-						for _, property := range strings.Split(value, ",") {
-							parts := strings.SplitN(property, "=", 2)
-							if len(parts) != 2 {
-								lintExpression := LintExpression{
-									Rule:    "wait-for-properties-annotation",
-									Type:    "error",
-									Path:    spec.Path,
-									Message: fmt.Sprintf("Failed to parse %s annotation key=value pair: %s", key, property),
-								}
-								lintExpressions = append(lintExpressions, lintExpression)
-								break
-							}
-							if parts[0] == "" {
-								lintExpression := LintExpression{
-									Rule:    "wait-for-properties-annotation",
-									Type:    "error",
-									Path:    spec.Path,
-									Message: fmt.Sprintf("Resource annotation %s should not have an empty jsonpath key: %s", key, property),
-								}
-								lintExpressions = append(lintExpressions, lintExpression)
-								break
-							}
-							if parts[1] == "" {
-								lintExpression := LintExpression{
-									Rule:    "wait-for-properties-annotation",
-									Type:    "error",
-									Path:    spec.Path,
-									Message: fmt.Sprintf("Resource annotation %s should not have an empty value: %s", key, property),
-								}
-								lintExpressions = append(lintExpressions, lintExpression)
-								break
-							}
-							if _, err := jsonpath.Parse("lint-jsonpath", fmt.Sprintf("{ %s }", parts[0])); err != nil {
-								lintExpression := LintExpression{
-									Rule:    "wait-for-properties-annotation",
-									Type:    "error",
-									Path:    spec.Path,
-									Message: fmt.Sprintf("Resource annotation %s should have a valid jsonpath key: %s", key, property),
-								}
-								lintExpressions = append(lintExpressions, lintExpression)
-								break
-							}
+				for _, property := range strings.Split(value, ",") {
+					parts := strings.SplitN(property, "=", 2)
+					if len(parts) != 2 {
+						lintExpression := LintExpression{
+							Rule:    "wait-for-properties-annotation",
+							Type:    "error",
+							Path:    spec.Path,
+							Message: fmt.Sprintf("Failed to parse %s annotation key=value pair: %s", key, property),
 						}
+						lintExpressions = append(lintExpressions, lintExpression)
+						break
+					}
+					if parts[0] == "" {
+						lintExpression := LintExpression{
+							Rule:    "wait-for-properties-annotation",
+							Type:    "error",
+							Path:    spec.Path,
+							Message: fmt.Sprintf("Resource annotation %s should not have an empty jsonpath key: %s", key, property),
+						}
+						lintExpressions = append(lintExpressions, lintExpression)
+						break
+					}
+					if parts[1] == "" {
+						lintExpression := LintExpression{
+							Rule:    "wait-for-properties-annotation",
+							Type:    "error",
+							Path:    spec.Path,
+							Message: fmt.Sprintf("Resource annotation %s should not have an empty value: %s", key, property),
+						}
+						lintExpressions = append(lintExpressions, lintExpression)
+						break
+					}
+					if _, err := jsonpath.Parse("lint-jsonpath", fmt.Sprintf("{ %s }", parts[0])); err != nil {
+						lintExpression := LintExpression{
+							Rule:    "wait-for-properties-annotation",
+							Type:    "error",
+							Path:    spec.Path,
+							Message: fmt.Sprintf("Resource annotation %s should have a valid jsonpath key: %s", key, property),
+						}
+						lintExpressions = append(lintExpressions, lintExpression)
+						break
 					}
 				}
 			}
