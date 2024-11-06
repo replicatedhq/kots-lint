221f17cd2293fa0d6e450b16fd2be13ac0f0ec09
Ricardo Maraschini <ricardo.maraschini@gmail.com>
2024-11-06 11:00:22 +0100

    feat: return error if ec version is a pre-release
    if the version present in the embedded cluster config is a pre-release,
    the linter should return an error.

--- pkg/ec/lint_test.go
+++ pkg/ec/lint_test.go
@@ -1,6 +1,8 @@
 package ec
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/replicatedhq/kots-lint/pkg/domain"
@@ -14,6 +16,7 @@ func Test_LintEmbeddedClusterVersion(t *testing.T) {
 		name      string
 		specFiles domain.SpecFiles
 		expect    []domain.LintExpression
+		apiResult []byte
 	}{
 		{
 			name: "valid version",
@@ -26,7 +29,8 @@ spec:
   version: "v1.2.2+k8s-1.29"`,
 				},
 			},
-			expect: []domain.LintExpression{},
+			expect:    []domain.LintExpression{},
+			apiResult: []byte(`{}`),
 		},
 		{
 			name: "invalid version",
@@ -47,10 +51,48 @@ spec:
 				},
 			},
 		},
+		{
+			name: "pre-release version",
+			specFiles: domain.SpecFiles{
+				{
+					Path: "",
+					Content: `apiVersion: embeddedcluster.replicated.com/v1beta1
+kind: Config
+spec:
+  version: "pre-release-version"`,
+				},
+			},
+			expect: []domain.LintExpression{
+				{
+					Rule:    "non-existent-ec-version",
+					Type:    "error",
+					Message: "Embedded Cluster version is a pre-release",
+				},
+			},
+			apiResult: []byte(`{"prerelease": true}`),
+		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
+			oldURL := githubAPIURL
+			defer func() { githubAPIURL = oldURL }()
+
+			server := httptest.NewServer(
+				http.HandlerFunc(
+					func(w http.ResponseWriter, r *http.Request) {
+						if test.apiResult == nil {
+							w.WriteHeader(http.StatusNotFound)
+							return
+						}
+						w.Header().Set("Content-Type", "application/json")
+						w.Write(test.apiResult)
+					},
+				),
+			)
+			defer server.Close()
+
+			githubAPIURL = server.URL
 			actual, err := LintEmbeddedClusterVersion(test.specFiles)
 			require.NoError(t, err)
 			assert.ElementsMatch(t, actual, test.expect)
